467e9d786849dae75e0c9054325e53218115afe5
Gabriel <gabrielgapseu@gmail.com>
2024-05-31 13:33:21 -0300

    style: change on textfield color

--- src/themes/StandartTheme.ts
+++ src/themes/StandartTheme.ts
@@ -1,27 +1,41 @@
 import { createTheme } from "@mui/material";
 
 export const StandartTheme = createTheme({
-    palette: {
-        primary: {
-            main: "#272727",
-            dark: "#4D4D4D",
-            light: '#CBCBCB',
-            contrastText: "#ffffff",
-        },
-        secondary: {
-            main: "#FFFFFF",
-            dark: "#D8D8D8",
-            contrastText: "#000000",
-        },
-        background: {
-            default: "#FFFFFF",
-            paper: "#272727"
-        }
+  palette: {
+    primary: {
+      main: "#272727",
+      dark: "#4D4D4D",
+      light: "#CBCBCB",
+      contrastText: "#ffffff",
     },
-    typography: {
-        allVariants: {
-          color: 'white'
-        }
-      }
+    secondary: {
+      main: "#FFFFFF",
+      dark: "#D8D8D8",
+      contrastText: "#000000",
+    },
+    background: {
+      default: "#FFFFFF",
+      paper: "#272727",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "white",
+    },
+  },
 
-})
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& label": {
+            color: "gray", // Cor da label quando está dentro do input
+          },
+          "& .MuiInput-underline:before": {
+            borderBottomColor: "gray",
+          },
+        },
+      },
+    },
+  },
+});
